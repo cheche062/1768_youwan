78218264c2be256e13d5dafee9345e348ece4ea5
cheche062 <yoojunglee@126.com>
2018-01-09 14:13:35 +0800

    async

---
+++ webpack/script/171210_TypeScript/es6/async/test1.ts
@@ -0,0 +1,28 @@
+/* function timeout(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function asyncPrint(value, ms) {
+    await timeout(ms);
+    console.log(value);
+}
+
+asyncPrint('hello world', 50).then(() => {
+    console.log('over~~')
+}); */
+
+
+async function timeout(ms) {
+    await new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function asyncPrint(value, ms) {
+    await timeout(ms);
+    console.log(value);
+}
+
+asyncPrint('hello world', 500);
--- webpack/script/171210_TypeScript/es6/async/test2.ts
+++ webpack/script/171210_TypeScript/es6/async/test2.ts
@@ -1,3 +1,5 @@
+import { type } from "os";
+
 var co = require('co');
 var fs = require('fs');
 
@@ -10,43 +12,24 @@ var readFile = function (fileName, option) {
     });
 };
 
-var gen = function* () {
+/* var gen = function* () {
     // 先读取字段
     var f1 = yield readFile('./2.json', 'utf-8');
     var key = JSON.parse(f1)[0];
     // console.log(key);
-    
+
     // 读取json对象
     var f2 = yield readFile('./1.json', 'utf-8');
     console.log(JSON.parse(f2)[key]);
+}; */
+
+const asyncReadFile = async function () {
+    const f1 = await readFile('../generator/2.json', 'utf-8');
+    var key = JSON.parse(f1)[0];
+    // console.log(key);
+
+    const f2 = await readFile('../generator/1.json', 'utf-8');
+    console.log(JSON.parse(f2)[key]);
 };
 
-co(gen).then(function () {
-    console.log('Generator 函数执行完成');
-});
-
-
-// 手动调用原理
-/* var g = gen();
-g.next().value.then((data) => {
-    g.next(data).value.then((data) => {
-        g.next(data);
-    })
-}) */
-
-/* function run(fn) {
-    var g = fn();
-
-    var next = (data) => {
-        var result = g.next(data);
-        if (result.done) {
-            console.log('执行完毕');
-        } else {
-            result.value.then(next);
-        }
-    }
-
-    next();
-}
-
-run(gen); */
+asyncReadFile();
