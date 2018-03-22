dbc64b42e89552c1816fac62dd227e5737e795c4
cheche062 <yoojunglee@126.com>
2018-03-22 10:11:22 +0800

    promise

--- webpack/script/171210_TypeScript/es6/promise/1.js
+++ webpack/script/171210_TypeScript/es6/promise/1.js
@@ -1,18 +1,12 @@
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('a');
-    }, 1000);
-}).then(function (value) {               
-    console.log("第一个" + value);
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(value + 'b');
-        }, 1000);
-    })
-}).then(function (value) {              
-    console.log("第二个" + value);
-}).then(function (value) {              
-    console.log("第三个" + value);
-})
+var p1 = Promise.resolve(1);
+var p2 = Promise.resolve(p1);
+var p3 = new Promise(function (resolve, reject) {
+    resolve(p1);
+});
 
+console.log(p1 === p2)
+console.log(p1 === p3)
 
+p1.then((value) => { console.log('p1=' + value)})
+p2.then((value) => { console.log('p2=' + value)})
+p3.then((value) => { console.log('p3=' + value)})
